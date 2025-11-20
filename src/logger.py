28e6431bd147407520f073c3b48e3c69c01dbf38
Jovalam <jose.valderrama.a@uni.pe>
2025-11-20 14:47:25 -0500

    Data_ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from datetime import datetime
-from logger import logging
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
@@ -11,6 +10,6 @@ LOG_FILE_PATH =os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format=" ",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
